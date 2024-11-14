c0f0be5609215f57a658066a1cb1ed47f1978756
Anshad UK <anshadu@gmail.com>
2024-11-14 19:46:42 +0530

    Merging two dictionaries

---
+++ MergingTwoDictionaries.py
@@ -0,0 +1,10 @@
+# dict1 = {"apple":3,"banana":1}
+# dict2 = {"orange":2,"pear":4}
+# merged_dict = {**dict1,**dict2}
+# print(merged_dict)
+
+list1 = [1,2,3,4,5]
+list2 = [6,7,8,9,10]
+
+merged_list = [*list1,*list2]
+print(merged_list)
