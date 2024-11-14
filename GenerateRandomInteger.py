7111f07295a0ce21cd27c3dd0859fbf42b074040
Anshad UK <anshadu@gmail.com>
2024-11-14 20:03:54 +0530

    Generate random integers

--- GenerateRandomInteger.py
+++ GenerateRandomInteger.py
@@ -1,3 +1,8 @@
-import random
-random_numbers = random.randint(1,10)
+# import random
+# random_numbers = random.randint(1,10)
+# print(random_numbers)
+
+import time
+seed = int(time.time()*1000000)
+random_numbers= (seed%10)+1
 print(random_numbers)
